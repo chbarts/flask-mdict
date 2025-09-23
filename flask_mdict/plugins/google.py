b7ac55d1ebe5d311ee53f02900181948d66566e7
Chris Barts <chbarts@gmail.com>
2025-09-23 03:47:23 -0600

    Translate to English

--- flask_mdict/plugins/google.py
+++ flask_mdict/plugins/google.py
@@ -9,7 +9,7 @@ def translate(content, item):
             content,
             translator='google',
             from_language=item.get('from_lan', 'auto'),
-            to_language=item.get('to_lan', 'zh'),
+            to_language=item.get('to_lan', 'en-US'),
         )
         return [text]
     except Exception as err:
